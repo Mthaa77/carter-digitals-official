d2bdbd51234f6a207de3d8ae9521a5c90c4cdeb7
Mthaa77 <tshepisokadiaka83@gmail.com>
2026-07-04 21:26:58 +0200

    feat: place BounceCards deck in Carter services experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { ServicesBounceDeck } from "./components/ServicesBounceDeck";
 import { BusinessSections } from "./components/business-sections";
 import { Welcome } from "./welcome";
 
 export default function Home() {
   return <>
     <Welcome />
+    <ServicesBounceDeck />
     <BusinessSections />
   </>;
 }
