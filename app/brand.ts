acdafe1d036d2651916450d727f4940ed7f18b4e
Mthaa77 <tshepisokadiaka83@gmail.com>
2026-07-04 18:58:46 +0200

    feat: add optimized Carter Digitals brand assets

---
+++ app/brand.ts
@@ -0,0 +1,3 @@
+export const brandLockup = "data:image/webp;base64,UklGRvp1AABXRUJQVlA4IO51AACwCgKdASqEA1oBPmEqkkckIiqlJdMcaVAMCWVBXarpmdHusIuyq4C+...";
+
+export const brandMark = "data:image/webp;base64,UklGR...";
